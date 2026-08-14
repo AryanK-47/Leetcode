ab24bb3e12a944519e284200c7e4c8ead141bd4b
Aryan Kush <aryankush2022@vitbhopal.ac.in>
2026-08-14 14:28:22 +0530

    Time: 16 ms (30.44%), Space: 14.9 MB (21.46%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int n=s.size();
+        
+    int n=s.size();
         unordered_map<char,int>mp;
 
        for(int i=0;i<n;i++){
         mp[s[i]]++;
        }
-       for(int i=0;i<n;i++){
+       int i=0;
+       while(i<n){
         if(mp[s[i]]==1)return i;
+        i++;
        }
         return -1;
     }
