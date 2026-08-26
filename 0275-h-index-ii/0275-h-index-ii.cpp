65ac4af0f88386624cadae61dfd7dad929f3eb89
Aryan Kush <aryankush2022@vitbhopal.ac.in>
2026-08-27 01:15:52 +0530

    Time: 4 ms (4.52%), Space: 31.6 MB (3.07%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool check(vector<int>arr,int n,int mid){
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>=mid){
+                count++;
+                if(count>=mid)return true;
+            }
+        }
+        return false;
+    }
+    int hIndex(vector<int>& citations) {
+        int n=citations.size();
+        int low=0;
+        int high=n;
+        int res=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(check(citations,n,mid)){
+                res=mid;
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+        return res;
+    }
+};
